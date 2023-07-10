513271c0b251c496315f27e869a17c48bc478bc7
Mark Foksha <mark.foksha1813@gmail.com>
2023-07-10 20:23:11 +0300

    Initial Commit

---
+++ Facade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Facade
+//
+//  Created by Марк Фокша on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
